b1b0f7ab0800fac45d4ca28133560f3b485120d2
Cyborion <majormaciek@outlook.com>
2019-12-29 14:25:04 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Grommet, Heading} from 'grommet'
 
 function App() {
+  /*A requirement for grommet is to import it and use it as a top-level node.
+     You can do this in App.js.*/
   return (
-    <div className="App">
-      
-    </div>
+    <Grommet className="App">
+      <Heading
+        size='xlarge'
+        color='#0070FF'>
+        Working!
+      </Heading>
+    </Grommet>
   );
 }
 
